191954300229d808a47598f56f434a3aa4d76a1f
Fernando Silva <fernandophes@gmail.com>
2025-09-10 16:31:20 -0300

    Remoção do Feign

--- src/main/java/br/org/kinflasy/MainApplication.java
+++ src/main/java/br/org/kinflasy/MainApplication.java
@@ -2,10 +2,8 @@ package br.org.kinflasy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class MainApplication {
 
 	public static void main(String[] args) {
