f1fc885c41d848f4c50f75da68d5c66582fe0382
Fernando Silva <fernandophes@gmail.com>
2025-09-12 01:14:00 -0300

    Remoção de logs desnecessários

--- src/main/java/br/org/kinflasy/clients/local/AddressLocalClient.java
+++ src/main/java/br/org/kinflasy/clients/local/AddressLocalClient.java
@@ -10,9 +10,7 @@ import br.org.kinflasy.clients.AddressClient;
 import br.org.kinflasy.libs.contacts.dto.AddressDto;
 import br.org.kinflasy.libs.contacts.dto.AddressRequest;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Component
 @AllArgsConstructor
 public class AddressLocalClient implements AddressClient {
@@ -21,31 +19,26 @@ public class AddressLocalClient implements AddressClient {
 
     @Override
     public List<AddressDto> listAll() {
-        log.info("\n\nLOCAL\n\n");
         return controller.listAll().getBody();
     }
 
     @Override
     public AddressDto create(AddressRequest request) {
-        log.info("\n\nLOCAL\n\n");
         return controller.create(request).getBody();
     }
 
     @Override
     public AddressDto findById(UUID id) {
-        log.info("\n\nLOCAL\n\n");
         return controller.findById(id).getBody();
     }
 
     @Override
     public AddressDto update(UUID id, AddressRequest request) {
-        log.info("\n\nLOCAL\n\n");
         return controller.update(id, request).getBody();
     }
 
     @Override
     public void delete(UUID id) {
-        log.info("\n\nLOCAL\n\n");
         controller.delete(id);
     }
 
