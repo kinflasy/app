336f69cee76e0ca3d0970b4e204b9e6773237c32
Fernando Silva <fernandophes@gmail.com>
2025-09-12 01:15:52 -0300

    Criação do cliente de usuários

---
+++ src/main/java/br/org/kinflasy/clients/UserClient.java
@@ -0,0 +1,24 @@
+package br.org.kinflasy.clients;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+import br.org.kinflasy.libs.people.dto.UserDto;
+import br.org.kinflasy.libs.people.dto.UserRequest;
+
+@Component
+public interface UserClient {
+
+    List<UserDto> listAll();
+
+    UserDto create(UserRequest request);
+
+    UserDto findById(UUID id);
+
+    UserDto update(UUID id, UserRequest request);
+
+    void delete(UUID id);
+
+}
