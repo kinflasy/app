30f27f93e6e172cc4167f66d8017e821004eae6f
Fernando Silva <fernandophes@gmail.com>
2026-01-03 23:33:23 -0300

    feat(modularization): :sparkles: separa o módulo da API de contatos

--- execution/src/main/java/br/org/kinflasy/MainApplication.java
+++ execution/src/main/java/br/org/kinflasy/MainApplication.java
@@ -3,7 +3,7 @@ package br.org.kinflasy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "br.org.kinflasy.*" })
 public class MainApplication {
 
 	public static void main(String[] args) {
