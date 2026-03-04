b78c9c7933abda662eba7a8a72cdefcb21302dc3
Fernando Silva <fernandophes@gmail.com>
2026-03-04 14:21:00 -0300

    refactor: :fire: remove HierarchyEnum

--- libs/churches/src/main/java/br/org/kinflasy/libs/churches/enums/department/IntegrationType.java
+++ libs/churches/src/main/java/br/org/kinflasy/libs/churches/enums/department/IntegrationType.java
@@ -1,23 +1,14 @@
 package br.org.kinflasy.libs.churches.enums.department;
 
-import java.util.Set;
-
-import br.org.kinflasy.libs.lib_utils.contracts.HierarchyEnum;
 import lombok.Getter;
 
 @Getter
-public enum IntegrationType implements HierarchyEnum<IntegrationType> {
+public enum IntegrationType {
 
     OBSERVER,
-    CONSULTANT(OBSERVER),
-    INTEGRANT(OBSERVER),
-    ASSISTANT(INTEGRANT),
-    LEADER(ASSISTANT);
-
-    private final Set<IntegrationType> includedValues;
-
-    private IntegrationType(final IntegrationType... values) {
-        this.includedValues = flatIncludedValues(values);
-    }
+    CONSULTANT,
+    INTEGRANT,
+    ASSISTANT,
+    LEADER;
 
 }
--- libs/churches/src/main/java/br/org/kinflasy/libs/churches/enums/membership/Affiliation.java
+++ libs/churches/src/main/java/br/org/kinflasy/libs/churches/enums/membership/Affiliation.java
@@ -1,21 +1,12 @@
 package br.org.kinflasy.libs.churches.enums.membership;
 
-import java.util.Set;
-
-import br.org.kinflasy.libs.lib_utils.contracts.HierarchyEnum;
 import lombok.Getter;
 
 @Getter
-public enum Affiliation implements HierarchyEnum<Affiliation> {
+public enum Affiliation {
 
     VISITOR,
-    CONGREGATED(VISITOR),
-    MEMBER(CONGREGATED);
-
-    private final Set<Affiliation> includedValues;
-
-    private Affiliation(final Affiliation... values) {
-        this.includedValues = flatIncludedValues(values);
-    }
+    CONGREGATED,
+    MEMBER;
 
 }
