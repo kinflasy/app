13dcff0de8ba88196a0c984a1e13bc92a5c6f302
Fernando Silva <fernandophes@gmail.com>
2026-03-06 23:38:30 -0300

    feat(target): :sparkles: aplica os DTOs de acesso aos DTOs de departamento

--- libs/churches/src/main/java/br/org/kinflasy/libs/churches/dto/departments/DepartmentDto.java
+++ libs/churches/src/main/java/br/org/kinflasy/libs/churches/dto/departments/DepartmentDto.java
@@ -1,9 +1,12 @@
 package br.org.kinflasy.libs.churches.dto.departments;
 
+import java.util.List;
 import java.util.UUID;
 
+import br.org.kinflasy.libs.churches.contracts.access_rules.UnitLevelRule;
 import br.org.kinflasy.libs.churches.enums.department.DepartmentType;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -11,10 +14,22 @@ import lombok.NoArgsConstructor;
 public class DepartmentDto {
 
     private UUID id;
+    private UUID unitId;
     private String name;
     private String slug;
-    private UUID unitId;
-    private UUID visibilityId;
     private DepartmentType type;
+
+    @Data
+    public static class Rules {
+        private List<UnitLevelRule> visibilityRules;
+        private List<UnitLevelRule> joinRules;
+    }
+
+    @Data
+    @EqualsAndHashCode(callSuper = true)
+    public static class WithRules extends DepartmentDto {
+        private List<UnitLevelRule> visibilityRules;
+        private List<UnitLevelRule> joinRules;
+    }
 
 }
--- libs/churches/src/main/java/br/org/kinflasy/libs/churches/dto/departments/DepartmentRequest.java
+++ libs/churches/src/main/java/br/org/kinflasy/libs/churches/dto/departments/DepartmentRequest.java
@@ -1,9 +1,13 @@
 package br.org.kinflasy.libs.churches.dto.departments;
 
+import java.util.List;
+
+import br.org.kinflasy.libs.churches.contracts.access_rules.UnitLevelRule;
 import br.org.kinflasy.libs.churches.enums.department.DepartmentType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -13,6 +17,18 @@ public class DepartmentRequest {
     private @NotBlank String name;
     private @NotBlank String slug;
     private @NotNull DepartmentType type;
-    // private ConditionRequest visibility;
+
+    @Data
+    public static class Rules {
+        private List<UnitLevelRule> visibilityRules;
+        private List<UnitLevelRule> joinRules;
+    }
+
+    @Data
+    @EqualsAndHashCode(callSuper = true)
+    public static class WithRules extends DepartmentRequest {
+        private List<UnitLevelRule> visibilityRules;
+        private List<UnitLevelRule> joinRules;
+    }
 
 }
