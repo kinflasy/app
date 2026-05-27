98c0eec82148ac683ff61f9a10ae2c6d5a37dab3
Fernando Silva <fernandophes@gmail.com>
2026-05-26 23:28:24 -0300

    feat(scales): :technologist: atribui subtipos de Json ao ScaleDto

--- libs/calendar/src/main/java/br/org/kinflasy/libs/calendar/dto/scales/ScaleDto.java
+++ libs/calendar/src/main/java/br/org/kinflasy/libs/calendar/dto/scales/ScaleDto.java
@@ -2,9 +2,17 @@ package br.org.kinflasy.libs.calendar.dto.scales;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import lombok.Data;
 
 @Data
+@JsonSubTypes({
+        @Type(name = "OWNER", value = OwnerScaleDto.class),
+        @Type(name = "COLLABORATOR", value = CollaboratorScaleDto.class) })
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 public class ScaleDto {
 
     private UUID id;
