f1d799451766d71b8ad8fbaaddd0c5d3f1e8cd06
Fernando Silva <fernandophes@gmail.com>
2026-05-26 23:23:44 -0300

    feat(scales): :construction: cria entidades de escalas

---
+++ apis/calendar/src/main/java/br/org/kinflasy/apis/calendar/entities/scales/CollaboratorScale.java
@@ -0,0 +1,28 @@
+package br.org.kinflasy.apis.calendar.entities.scales;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+@Entity
+@Table(name = "collaborator_scales")
+@DynamicUpdate
+@Data
+@Accessors(chain = false)
+@EqualsAndHashCode(callSuper = true)
+public class CollaboratorScale extends Scale {
+
+    /*
+     * Chaves "estrangeiras" (referências)
+     */
+    @Column(nullable = false)
+    private UUID collaborationId;
+
+}
---
+++ apis/calendar/src/main/java/br/org/kinflasy/apis/calendar/entities/scales/Scale.java
@@ -0,0 +1,44 @@
+package br.org.kinflasy.apis.calendar.entities.scales;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import br.org.kinflasy.libs.api_utils.AbstractSimpleAuditable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+@Entity
+@DynamicUpdate
+@Table(name = "calendar_events")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Data
+@Accessors(chain = false)
+@EqualsAndHashCode(callSuper = false)
+public abstract class Scale extends AbstractSimpleAuditable {
+
+    /*
+     * Chave primária
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    /*
+     * Chaves "estrangeiras" (referências)
+     */
+
+    @Column(nullable = false)
+    private UUID lineupId;
+
+}
